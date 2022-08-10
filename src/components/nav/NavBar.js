31fa64d354536e9454b54e6d86319adeca8408b4
Samantha Malorni <samantha.ann.malorni@gmail.com>
2022-08-10 16:58:35 -0400

    initial commit

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,34 @@
+import { Link, useNavigate } from "react-router-dom"
+import "./NavBar.css"
+
+export const NavBar = () => {
+    const navigate = useNavigate()
+    return (
+        <ul className="navbar">
+            <li className="navbar__item">
+                <Link to="/games"></Link>
+            </li>
+            <li className="navbar__item">
+                <Link to="/events"></Link>
+            </li>
+            {
+                (localStorage.getItem("lu_token") !== null) ?
+                    <li className="nav-item">
+                        <button className="nav-link fakeLink"
+                            onClick={() => {
+                                localStorage.removeItem("lu_token")
+                                navigate('/login')
+                            }}
+                        >Logout</button>
+                    </li> :
+                    <>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/register">Register</Link>
+                        </li>
+                    </>
+            }        </ul>
+    )
+}
